987b2c1931b812a26ab29a7da257127f0f08a123
AnanyaA148 <138643111+AnanyaA148@users.noreply.github.com>
2024-10-31 15:28:32 -0400

    occurance_temp reinitialized to 0 after every loop

--- UNIT6HW.c
+++ UNIT6HW.c
@@ -25,14 +25,18 @@ char most_occuring_character ( char *str){
     //^
 
 	for (int i = 0; i< size-1; i++){
+        occurance_temp =0;
         for (int i2 = i+1; i2<size; i2++){
             if (str[i]== *(str+i2)){
                 occurance_temp++;
             }
         }
         if (occurance_temp!=0&&occurance_temp>occurance+1){
-                occurance=occurance_temp;
-                popChar = str[i];
+                if (str[i] != ' '){
+                    occurance=occurance_temp;
+                    popChar = str[i];
+                }
+
 
         }
 	}
