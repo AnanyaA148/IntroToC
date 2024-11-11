de90716acd894c84a356dfc17da136edea827332
AnanyaA148 <138643111+AnanyaA148@users.noreply.github.com>
2024-11-11 10:22:53 -0500

    No need for the & signs with the char pointers as the string is already a pointer

--- FinalProject.c
+++ FinalProject.c
@@ -22,15 +22,25 @@ void addFaculty(faculty * f); // Add a new faculty memeber
 
 void addFaculty(faculty * f){
 
+    f->name = malloc(50 * sizeof(char)); // Allocate enough space for the name
+    f->id = malloc(20 * sizeof(char)); // Allocate enough space for the id
+    f->department = malloc(50 * sizeof(char)); // Allocate enough space for the department
+    f->rank = malloc(30 * sizeof(char));
+
     printf("\n\nEnter the faculty's info:\n\n ");
+
     printf("\tName of the faculty: ");
-    scanf("%s", &(*f).name);
+    scanf("%s", (*f).name);
+
     printf("\tID: ");
-    scanf("%s", &(*f).id);
+    scanf("%s", (*f).id);
+
     printf("\tRank:");
-    scanf("%s", &(*f).rank);
+    scanf("%s", (*f).rank);
+
     printf("\tDepartment: ");
-    scanf("%s", &(*f).department);
+    scanf("%s", (*f).department);
+
 
 
 }
@@ -39,17 +49,51 @@ void addStudent(student * s); // Add a new student
 
 void addStudent(student * s){
 
+    s->name = malloc(50 * sizeof(char)); // Allocate enough space for the name
+    s->id = malloc(20 * sizeof(char)); // Allocate enough space for the id
+
+
     printf("\n\nEnter the student's info:\n\n ");
+
     printf("\tName of the student: ");
-    scanf("%s", &(*s).name);
+    scanf("%s", (*s).name);
+    printf("%s", s->name);
+
     printf("\tID: ");
-    scanf("%s", &(*s).id);
+    scanf("%s", (*s).id);
+
     printf("\tGPA:");
-    scanf("%s", &(*s).gpa);
+    scanf("%f", &(*s).gpa);
+    printf("%f", (*s).gpa);
+
     printf("\tCredit hours: ");
-    scanf("%s", &(*s).credit);
+    scanf("%d", &(*s).credit);
 
 
+}
+
+void printTuition(student *s); // Print tuition invoice for a student
+
+void printTuition(student *s){
+    char * givenId;
+    printf("Enter the student's id");
+    scanf("%s", givenId);
+    printf("hi");
+
+
+    for(student * p = s; p<s+5; p++){
+        printf("hi");
+        if (strcmp((*p).id, givenId)){
+            printf("bye");
+            printf("Here is the tuition invoice for %s", (*p).name);
+            printf("%s \t\t\t\t %s", (*p).name, (*p).id);
+            printf("Credit Hours: %d ($236.45/credit hour)", (*p).credit);
+            printf("Fees: $52");
+            printf("Total Payment:");
+
+            break;
+        }
+    }
 }
 
  int main(){
@@ -85,13 +129,15 @@ void addStudent(student * s){
                 numstudent++;
                 break;
             case 3:
+                printTuition(studentlst);
                 break;
             case 4:
                 break;
             case 5:
                 done =1;
                 break;
-
+            default:
+                done =1;
 
 
         }
