45c8bbbd47acfa99fe0dacf04ccb2c7a75784062
AnanyaA148 <138643111+AnanyaA148@users.noreply.github.com>
2024-11-10 20:18:21 -0500

    Created a switch for the user selection
    The switch works

---
+++ FinalProject.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+ int main(){
+
+
+    while(1){
+        int done =0;
+        printf("1- Add a new faculty member\n");
+        printf("2- Add a new Student \n");
+        printf("3- Print tuition invoice for a student\n");
+        printf("4- Print information of a faculty\n");
+        printf("5 - Exit Program\n");
+        printf("Enter your selection\n");
+        int select;
+        scanf("%d", &select);
+
+        switch(select)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                done =1;
+                break;
+
+
+
+        }
+        if (done){
+            break;
+        }
+
+
+    }
+ }
