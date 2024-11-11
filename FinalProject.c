1b902d9b15e830e14abb7dddef5a314cc925074e
AnanyaA148 <138643111+AnanyaA148@users.noreply.github.com>
2024-11-10 20:47:28 -0500

    Fixed formatting

--- FinalProject.c
+++ FinalProject.c
@@ -17,23 +17,49 @@ typedef struct{ // student info
 
 } student;
 
+
+void addFaculty(faculty * f); // Add a new faculty memeber
+
+void addFaculty(faculty * f){
+
+    printf("\n\nEnter the faculty's info:\n\n ");
+    printf("\tName of the faculty: ");
+    scanf("%s", &(*f).name);
+    printf("\tID: ");
+    scanf("%s", &(*f).id);
+    printf("\tRank:");
+    scanf("%s", &(*f).rank);
+    printf("\tDepartment: ");
+    scanf("%s", &(*f).department);
+
+
+}
+
  int main(){
 
+    faculty * facultylst;
+    facultylst = malloc(sizeof(faculty));
+    int numfaculty =0;
+
+    student * studentlst;
 
     while(1){
         int done =0;
+        printf("Choose one of the options: \n\n");
         printf("1- Add a new faculty member\n");
         printf("2- Add a new Student \n");
         printf("3- Print tuition invoice for a student\n");
         printf("4- Print information of a faculty\n");
         printf("5 - Exit Program\n");
-        printf("Enter your selection\n");
+        printf("\n\n\tEnter your selection: ");
         int select;
         scanf("%d", &select);
 
         switch(select)
         {
             case 1:
+                addFaculty(facultylst + numfaculty);
+                numfaculty++;
                 break;
             case 2:
                 break;
