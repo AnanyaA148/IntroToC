548fe8e901f03fde32cea6b5fdf868a7741035b2
AnanyaA148 <138643111+AnanyaA148@users.noreply.github.com>
2024-11-16 12:26:38 -0500

    Update unit8hw.c

--- unit8hw.c
+++ unit8hw.c
@@ -43,6 +43,51 @@ returns 1 if name is found in list. Otherwise, it returns 0
 //5.
 float highestNetSalary ( struct employee * list);
 /*
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+struct employee {
+    int id;
+    char name[50];
+    float salary;
+    int numberOfDependents;
+    //---------------------
+    struct employee * next;
+    struct employee * below;
+};
+
+//----------------------- FUNCTION PROTOTYPES ---------------------------
+//0.
+int getId ( char * name);
+/*
+returns the sum of the ascii codes of the letters of name.
+*/
+//1.
+void printEmployee ( struct employee e);
+/*
+prints an employee in the format [id,name,net salary], where:
+Net salary = salary*0.91 + (numberOfDependent *0.01*salary)
+Examples: [562,AaBc xYz,1253.56] [562,bAac ZyX,6253.06]
+*/
+//2.
+void printAllEmployees ( struct employee * list);
+/*
+prints all employees in the list. Loops through list and calls
+printEmployee for each employee in list
+*/
+//3.
+struct employee * addEmployee ( struct employee * list , struct employee e);
+/*
+adds e to list.
+*/
+//4.
+int searchEmployee ( struct employee * list , char * name);
+/*
+returns 1 if name is found in list. Otherwise, it returns 0
+*/
+//5.
+float highestNetSalary ( struct employee * list);
+/*
 returns the highest net salary.
 */
 //----------------------- FUNCTION DEFINITIONS ---------------------------
@@ -51,8 +96,11 @@ int getId ( char * name ){
     int sum;
     int num;
     for(int i =0; name[i] != '\0'; i++){
-        num = name[i] = '0';
+        num = name[i] - '0';
         sum = sum + num;
+        if (name[i] == '\n'){
+            name[i] = '\0';
+        }
     }
 
     return sum;
