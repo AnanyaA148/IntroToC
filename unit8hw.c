96ecf8b269f149b9a45615466e36bd30c86bfbb3
AnanyaA148 <138643111+AnanyaA148@users.noreply.github.com>
2024-11-16 00:23:16 -0500

    Made all the functions

--- unit8hw.c
+++ unit8hw.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 #include<string.h>
 struct employee {
-int id;
-char name[50];
-float salary;
-int numberOfDependents;
-//---------------------
-struct employee * next;
-struct employee * below;
+    int id;
+    char name[50];
+    float salary;
+    int numberOfDependents;
+    //---------------------
+    struct employee * next;
+    struct employee * below;
 };
+
 //----------------------- FUNCTION PROTOTYPES ---------------------------
 //0.
 int getId ( char * name);
@@ -17,7 +18,7 @@ int getId ( char * name);
 returns the sum of the ascii codes of the letters of name.
 */
 //1.
-void printEmployee ( struct employee );
+void printEmployee ( struct employee e);
 /*
 prints an employee in the format [id,name,net salary], where:
 Net salary = salary*0.91 + (numberOfDependent *0.01*salary)
@@ -47,61 +48,98 @@ returns the highest net salary.
 //----------------------- FUNCTION DEFINITIONS ---------------------------
 //0.
 int getId ( char * name ){
-    printf ("COMING UP\n");
-    return 0;
+    int sum;
+    int num;
+    for(int i =0; name[i] != '\0'; i++){
+        num = name[i] = '0';
+        sum = sum + num;
+    }
+
+    return sum;
 }
 //1.
-void printEmployee ( struct employee ){
-    printf ("COMING UP\n");
+void printEmployee ( struct employee e){
+
+    float netSalary = e.salary*0.91 + (e.numberOfDependents *0.01*e.salary);
+    printf("[%d,%s,%f]", e.id, e.name, netSalary);
+
 }
 //2.
 void printAllEmployees ( struct employee * list){
-    printf ("COMING UP\n");
+
+    while(list != NULL){
+        printEmployee(*list);
+        list = list->next;
+    }
 }
 //3.
 struct employee * addEmployee ( struct employee * list , struct employee e){
-    printf ("COMING UP\n");
+
+    while(list != NULL){
+        list = list->next;
+    }
+    list->next = &e;
     return list;
 }
 
 //4.
 int searchEmployee ( struct employee * list , char * name){
-    printf ("COMING UP\n");
+    while(list != NULL){
+        if (strcmp(list->name, name) ==0){
+            return 1;
+        }
+        list = list->next;
+    }
     return 0;
+
 }
 //5.
 float highestNetSalary ( struct employee * list){
-    printf ("COMING UP\n");
-    return 0.0;
+    float high = 0;
+    while(list != NULL){
+        float netSalary = list->salary*0.91 + (list->numberOfDependents *0.01*list->salary);
+        if (netSalary> high){
+            high = netSalary;
+        }
+        list = list->next;
+    }
+
+    return high;
 }
 //DO NOT CHANGE ANYTHING IN main
 int main() {
     struct employee * company = NULL , tempEmployee;
     int tempInt = 0;
     char tempName [50] = "Allal Kamch" ;
-
     char dummy;
+
+
     printf ("Enter 1 to add a new employee, 0 to stop: ");
     scanf ("%d", &tempInt);
 
     while ( tempInt == 1 ){
+
         printf("Enter Full Name: ");
         scanf("%c",&dummy);
-
         fgets(tempEmployee.name, 50, stdin);
+
         tempEmployee.id = getId ( tempEmployee.name );
 
         printf ("Enter salary-numberOfDependents: ");
-        scanf ("%f-%d", &tempEmployee.salary,
-        &tempEmployee.numberOfDependents);
+        scanf ("%f-%d", &tempEmployee.salary, &tempEmployee.numberOfDependents);
+
         company = addEmployee ( company , tempEmployee );
         printf ("Enter 1 to add a new employee, 0 to stop: ");
         scanf ("%d", &tempInt);
     }
+
     printAllEmployees ( company ) ;
+
     printf ("Highest Net Salary = %.2f\n", highestNetSalary(company) );
+
     tempInt = searchEmployee ( company , tempName) ;
     if ( tempInt == 1 ) printf ("%s is found!\n", tempName);
     else printf ("%s is NOT found!\n", tempName);
+
     return 0;
 }
